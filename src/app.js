c17b4e28474f92a05c0cf37a705a500da6e086c8
Pratik <pgumthanavar05@gmail.com>
2025-04-05 17:31:22 +0530

    Add new routes for '/hello/2' and '/test'

--- src/app.js
+++ src/app.js
@@ -4,12 +4,25 @@ const app = express();
 
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('This is a homepage !');
+app.use(
+  '/hello/2',
+  (req, res, next) => {
+    console.log('Router 1');
+    // res.send('Hello world 2 !');
+    next();
+  },
+  (req, res) => {
+    console.log('Router 2');
+    res.send('Hello world !');
+  }
+);
+
+app.use('/test', (req, res, next) => {
+  res.send('Test Page');
 });
 
-app.get('/test', (req, res) => {
-  res.send('Hello world !');
+app.use('/', (req, res) => {
+  res.send('This is a homepage !');
 });
 
 app.listen(port, () => {
