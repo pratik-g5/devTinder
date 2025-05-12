a51bf6ff68e0cab1d2455d476a839ec827e49c69
Pratik <pgumthanavar05@gmail.com>
2025-05-12 14:36:29 +0530

    add pagination support to user feed endpoint

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -61,7 +61,11 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 
 userRouter.get('/user/feed', userAuth, async (req, res) => {
   try {
-    loggedInUser = req.user;
+    const loggedInUser = req.user;
+    const page = req.query.page || 1;
+    let limit = req.query.limit || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
 
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -78,8 +82,14 @@ userRouter.get('/user/feed', userAuth, async (req, res) => {
         { _id: { $ne: loggedInUser._id } },
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
       ],
-    });
+    })
+      .select(USER_DATA)
+      .skip(skip)
+      .limit(limit);
 
+    if (users.length === 0) {
+      throw new Error('No users found!');
+    }
     res.send(users);
   } catch (err) {
     res.status(500).send('ERROR: ' + err.message);
